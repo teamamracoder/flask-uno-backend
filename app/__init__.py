23cbf02cf620378a8e7f5b168278c2242dc6e78c
Protap Barman <protapbarman02@gmail.com>
2024-07-14 21:29:25 +0530

    added cors in requirements

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from .routes import register_blueprints
 from db import mongo
 from config import Config
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     app.config.from_object(Config)
     mongo.init_app(app)
