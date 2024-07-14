885fd275d810abdf93aced0bb7686e12e70dc1b3
Protap Barman <protapbarman02@gmail.com>
2024-07-14 23:12:23 +0530

    aa

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 class Config:
-    MONGO_URI = 'mongodb+srv://teamamracoder:tS2Vkx5LoQZORm5g@amracodercloud.i4qjfv9.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=AmraCoderCloud'
+    MONGO_URI = 'mongodb+srv://yoyobprotap:pF0D7vnUzp5vPrJY@protap.5uqnmyw.mongodb.net/sample_mflix?'
+    # MONGO_URI = 'mongodb+srv://teamamracoder:tS2Vkx5LoQZORm5g@amracodercloud.i4qjfv9.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=AmraCoderCloud'
