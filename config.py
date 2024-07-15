611e708dd80408d03f69768e285af151893ccacb
Protap Barman <protapbarman02@gmail.com>
2024-07-15 14:35:57 +0530

    connection url

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
 class Config:
     MONGO_URI = 'mongodb+srv://yoyobprotap:pF0D7vnUzp5vPrJY@protap.5uqnmyw.mongodb.net/sample_mflix?'
-    # MONGO_URI = 'mongodb+srv://teamamracoder:tS2Vkx5LoQZORm5g@amracodercloud.i4qjfv9.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=AmraCoderCloud'
